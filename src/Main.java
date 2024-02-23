b51dd02fb084fbcb58253586a2fea18174697e08
IlyaShukshin <freevhip88@mail.ru>
2024-02-23 22:35:14 +0300

    complete 2 task

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
 public class Main {
     public static void main (String [] srgs) {
         System.out.println("Задача №1");
+        int age1 = 22, age2 = 16;
+        if (age1 >= 18) {
+            System.out.println("Если возраст человека равен, " + age1 + " то он совершеннолетний");
+        }
+        if (age2 < 18){
+            System.out.println("Если возраст человека равен, " + age2 + " то он не достиг совершенолетия");
+        }
+        System.out.println("Задача №2");
+
+        int temp1 = -30;
+        if (temp1 < -5) {
+            System.out.println("На улице " + temp1 + " градусов, нужно надеть шапку");
+        }
+        int temp2 = 40;
+        if (temp2 > 5) {
+            System.out.println("На улице " + temp2 + " градусов, можно идти без шапки");
+        }
+        System.out.println("Задача №3");
 
     }
+
+
 }
+
+
